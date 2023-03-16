dc14462237724b3acf0eb189cd7a96cc545e4036
gstv <gustavdlim@gmail.com>
2023-03-16 20:54:45 -0300

    feat: items added in database and front with categories

--- marketplace/item/models.py
+++ marketplace/item/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
     class Meta:
+        ordering = ('name', )
         verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images/', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('name', )
 
     def __str__(self):
         return self.name
